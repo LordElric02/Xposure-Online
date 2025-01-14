4f4497abbf4bec17f7695ca993f1b66fd9f2183e
LordElric02 <elriclord02@gmail.com>
2025-01-14 11:14:10 -0600

    fixing checkins

--- frontend/src/components/ThumnnailGallery.js
+++ frontend/src/components/ThumnnailGallery.js
@@ -8,13 +8,13 @@ export const ThumbnailGallery = ({ videos, handleThumbnailClick }) => {
     <Grid2 container spacing={2}>
       {videos.map((video, index) => (
         <Grid2 item xs={2} key={index}> {/* Set width for 6 items per row */}
-          <Typography variant="h5" gutterBottom>{video.videoTitle}</Typography> {/* Use Material-UI Typography */}
+          <Typography variant="h5" gutterBottom>{video.title}</Typography> {/* Use Material-UI Typography */}
           <Paper elevation={4}>
             <img
               src={video.thumbnailUrl}
               alt={`Thumbnail ${index + 1}`}
               style={{ width: '150px', height: 'auto', cursor: 'pointer' }}
-              onClick={() => handleThumbnailClick(video.videoUrl)}
+              onClick={() => handleThumbnailClick(video.videoUrl,video.title)}
             />
           </Paper>
         </Grid2>

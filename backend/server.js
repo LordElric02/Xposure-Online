bdb5826316a0ccf4d6cce11d887197c67635b0ff
LordElric02 <elriclord02@gmail.com>
2025-01-20 14:57:18 -0600

    created first build/deploy

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,30 @@
 import express from "express";
+import path from "path";
 import cors from  "cors";
 import videoRoutes from "./Routes/videoRoutes.js";
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const filePath = 'videos/Screen_Recording_20241211_115130_WatchClubTV.mp484bd0119-a983-4354-b91a-fc7400095718';
 const app = express();
 app.use (cors());
-app.use(express.json({ limit: '10mb' })); // Adjust limit as needed
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json({ limit: '10mb' })); // Adjust limit as
+//  needed
 
 app.use('/api/videos', videoRoutes);
 
-app.listen(5000, () => {            
-  console.log("Server started on port 5000");        
-});     
+// The "catchall" handler: for any request that doesn't
+// match one above, send back the React app.
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});

f001de6c23171920e147491412c71f4368b3bb5c
LordElric02 <elriclord02@gmail.com>
2025-01-21 16:33:46 -0600

    fixing biuld/deploy

--- frontend/src/components/PublicVideos.js
+++ frontend/src/components/PublicVideos.js
@@ -18,7 +18,7 @@ const  PublicVideos = ({ refreshVideos }) => {
 
       try {
         const videoFetchPromises = vgroups.map(async (group) => {
-          console.log(`window.location.pathname: ${window.location.pathname}`); // Log the window.location.hostnam
+          console.log(`window.location.hostname: ${window.location.hostname}`); // Log the window.location.hostnam
           const isRunningInsideBackend = ((window.location.port === '5000') && (window.location.hostname === 'localhost')) || (window.location.hostname === 'xposure-online.onrender.com');    
           let videoEndpoint = ``;
           if (!isRunningInsideBackend) {

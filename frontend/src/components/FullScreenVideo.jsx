99005647ab990ee90ed136efc8b8a822af20788c
LordElric02 <elriclord02@gmail.com>
2025-01-27 11:33:55 -0600

    updating video player

--- frontend/src/components/FullScreenVideo.jsx
+++ frontend/src/components/FullScreenVideo.jsx
@@ -3,23 +3,21 @@ import React from 'react';
 import Typography from '@mui/material/Typography'; // Import Typography from Material-UI
 import { useSelector } from 'react-redux'; // Import useSelector
 
-
 const FullScreenVideo = () => {
-  const videoUrl = useSelector((state) => state.video.videoUrl); // Get user info from Redux store{{
+  const videoUrl = useSelector((state) => state.video.videoUrl); // Get user info from Redux store
   const videoName = useSelector((state) => state.video.videoName);
   console.log(`videoUrl: ${videoUrl}`);
   console.log(`videoName: ${videoName}`);
-  
- 
+
   return (
     <div style={styles.videoContainer}>
-      Full screen video player
       <Typography variant="h5" gutterBottom>{videoName}</Typography>
       <video
         controls
         style={styles.video}
         src={videoUrl}
         autoPlay
+        onContextMenu={(e) => e.preventDefault()} // Optional: Prevent context menu
       >
         Your browser does not support the video tag.
       </video>
@@ -31,7 +29,7 @@ const styles = {
   videoContainer: {
     position: 'relative',
     width: '100%',
-    maxHeight: '60vh', // Optional: restrict maximum height
+    maxHeight: '120vh', // Optional: restrict maximum height
     overflow: 'hidden', // Prevent overflow
   },
   video: {
